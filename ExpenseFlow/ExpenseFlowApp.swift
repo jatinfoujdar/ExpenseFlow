04b1c90c74f030fc3e64248733213b1372289503
Jatin Foujdar <jatin321001@gmail.com>
2025-12-17 17:51:34 +0530

    Initial Commit

---
+++ ExpenseFlow/ExpenseFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseFlowApp.swift
+//  ExpenseFlow
+//
+//  Created by jatin foujdar on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
